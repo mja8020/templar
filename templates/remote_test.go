f15dadb99a9064d94e3ed8614204f401a794b5bf
mjbyter <6097343+mjbyter@users.noreply.github.com>
2024-10-25 21:11:58 -0400

    Render tests and values

--- templates/remote_test.go
+++ templates/remote_test.go
@@ -16,6 +16,7 @@ func TestRender(t *testing.T) {
 		err      string
 	}
 
+	// TODO: So many more, maybe build this fizture set somewhere else?
 	fixtures := []fixture{}
 
 	fixtures = append(fixtures, fixture{
@@ -27,15 +28,46 @@ func TestRender(t *testing.T) {
 		err:      "",
 	})
 
+	fixtures = append(fixtures, fixture{
+		name:     "values",
+		template: `{{ .FOO }}`,
+		values: map[string]interface{}{
+			"FOO": "bar",
+		},
+		expected: "bar",
+		sources:  map[string]DataSource{},
+		err:      "",
+	})
+
+	fixtures = append(fixtures, fixture{
+		name:     "values_combine",
+		template: `{{ .FOO | toUpper }}`,
+		values: map[string]interface{}{
+			"FOO": "bar",
+		},
+		expected: "BAR",
+		sources:  map[string]DataSource{},
+		err:      "",
+	})
+
+	fixtures = append(fixtures, fixture{
+		name:     "error",
+		template: `{{ !!!!! }}`,
+		values:   map[string]interface{}{},
+		expected: "",
+		sources:  map[string]DataSource{},
+		err:      "unexpected",
+	})
+
 	for _, fixture := range fixtures {
 		output, err := Render(fixture.template, fixture.values, fixture.sources)
 
 		if fixture.err == "" {
-			assert.Empty(t, err, "expected no error but got %s", err)
+			assert.Empty(t, err, "expected no error but got `%s`", err)
 		} else {
-			assert.ErrorContainsf(t, err, fixture.err, "unexpected error %s", err)
+			assert.ErrorContainsf(t, err, fixture.err, "unexpected error `%s`", err)
 		}
 
-		assert.Equal(t, fixture.expected, output, "template %s did not match, expected `%s` got `%s", fixture.name, fixture.expected, output)
+		assert.Equal(t, fixture.expected, output, "template %s did not match, expected `%s` got `%s`", fixture.name, fixture.expected, output)
 	}
 }
