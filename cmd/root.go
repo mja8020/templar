0d4d66f002377e1e5b710d2024bc7e56c53ec130
M <6097343+mja8020@users.noreply.github.com>
2024-10-25 21:33:48 -0400

    Add rootCmdFlags and target option (#12)

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -10,7 +9,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type rootCmdFlags struct {
+	target string
+}
 
+var RootCmdFlags rootCmdFlags
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -37,15 +40,12 @@ func Execute() {
 }
 
 func init() {
+	RootCmdFlags = rootCmdFlags{}
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.templar.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringVarP(&RootCmdFlags.target, "target", "t", "", "")
 }
-
-
