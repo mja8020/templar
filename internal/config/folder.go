2db063bb32df0e4e870a86100d3d8f63c20b51b9
mja8020 <6097343+mja8020@users.noreply.github.com>
2024-10-27 21:26:39 +0000

    Layers, folders etc

--- internal/config/folder.go
+++ internal/config/folder.go
@@ -7,10 +7,17 @@ type Folder struct {
 	Values      map[string]interface{}
 	Datasources map[string]templates.DataSource
 	Commands    map[string]Command
+	Templates   map[string]Template
 }
 
 type Command struct {
 	Environment map[string]string // Environment variables (values are templates)
 	Command     string            // The comand to run i.e. terraform plan (values are templates)
 	Success     []int             // Exit codes that are considered successfull
+}
+
+// Templates are considered configuration and are loaded with the rest of the configs, for now
+// only content is kept but we may decide later to add options for the templates i.e. IgnoreChanges
+type Template struct {
+	Content string
 }
--- internal/config/root.go
+++ internal/config/root.go
@@ -3,8 +3,14 @@ package config
 // Maybe workspace?
 type Root struct {
 	Folder
-	Layers    map[string]Layer    // Layers are only defined in root
+
+	// Settings only applicable in root
+	Layers    []Layer             // Layers are only defined in root, first is considered default
 	Variables map[string]Variable // Optionally define what values to expect
+	// The key is the folder path relative to the root? If we use a tree structure here with references
+	// to parent/child we won't be able to serialize.  We can possibly store the Tree as just a tree
+	// of node names with the folder values kept here.
+	Folders map[string]Folder
 }
 
 type Variable struct {
@@ -13,4 +19,7 @@ type Variable struct {
 	Default    interface{}
 }
 
-type Layer struct{}
+type Layer struct {
+	Levels []string
+	Match  string // Determines which layer is selected?
+}
