e682ca6c578831c33667835663e1d6d92ea6c8f1
mja8020 <6097343+mja8020@users.noreply.github.com>
2024-10-28 18:52:38 +0000

    Initial confdig loading

--- internal/config/root.go
+++ internal/config/root.go
@@ -1,8 +1,13 @@
 package config
 
+import "github.com/mja8020/templar/internal/tree"
+
 // Maybe workspace?
 type Root struct {
 	Folder
+
+	Path   string `yaml:"-"`
+	Target string `yaml:"-"`
 
 	// Settings only applicable in root
 	Layers    []Layer             `yaml:"layers,omitempty"`    // Layers are only defined in root, first is considered default
@@ -11,6 +16,11 @@ type Root struct {
 	// to parent/child we won't be able to serialize.  We can possibly store the Tree as just a tree
 	// of node names with the folder values kept here.
 	Folders map[string]Folder `yaml:"folders,omitempty"`
+
+	// Don't serialize, maybe we want to enable later?
+	RootConfig     string `yaml:"-"`
+	Folderconfig   string `yaml:"-"`
+	TemplateFolder string `yaml:"-"`
 }
 
 type Variable struct {
@@ -22,4 +32,23 @@ type Variable struct {
 type Layer struct {
 	Levels []string `yaml:"levels,omitempty"`
 	Match  string   `yaml:"match,omitempty"` // Determines which layer is selected?
+}
+
+func NewRoot(target string) (root Root, err error) {
+	root = Root{}
+
+	root.RootConfig = "templar.yaml"
+	root.Folderconfig = ".templar.yaml"
+	root.TemplateFolder = ".templar"
+
+	root.Target = target
+
+	tree, err := tree.NewTree(target)
+	if err != nil {
+		return
+	}
+
+	root.Path = tree.Root.Name
+
+	return
 }
