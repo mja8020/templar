39d0566cae8af9d3d805ecd7339cb21313a15f7c
mja8020 <6097343+mja8020@users.noreply.github.com>
2024-10-28 01:30:04 +0000

    yaml struct tags

--- internal/config/folder.go
+++ internal/config/folder.go
@@ -4,20 +4,20 @@ import "github.com/mja8020/templar/internal/templates"
 
 // Maybe project?
 type Folder struct {
-	Values      map[string]interface{}
-	Datasources map[string]templates.DataSource
-	Commands    map[string]Command
-	Templates   map[string]Template
+	Values      map[string]interface{}          `yaml:"values,omitempty"`
+	DataSources map[string]templates.DataSource `yaml:"datasources,omitempty"`
+	Commands    map[string]Command              `yaml:"commands,omitempty"`
+	Templates   map[string]Template             `yaml:"templates,omitempty"`
 }
 
 type Command struct {
-	Environment map[string]string // Environment variables (values are templates)
-	Command     string            // The comand to run i.e. terraform plan (values are templates)
-	Success     []int             // Exit codes that are considered successfull
+	Environment map[string]string `yaml:"environment,omitempty"` // Environment variables (values are templates)
+	Command     string            `yaml:"command,omitempty"`     // The comand to run i.e. terraform plan (values are templates)
+	Success     []int             `yaml:"success,omitempty"`     // Exit codes that are considered successfull
 }
 
 // Templates are considered configuration and are loaded with the rest of the configs, for now
 // only content is kept but we may decide later to add options for the templates i.e. IgnoreChanges
 type Template struct {
-	Content string
+	Content string `yaml:"content,omitempty"`
 }
--- internal/config/root.go
+++ internal/config/root.go
@@ -5,21 +5,21 @@ type Root struct {
 	Folder
 
 	// Settings only applicable in root
-	Layers    []Layer             // Layers are only defined in root, first is considered default
-	Variables map[string]Variable // Optionally define what values to expect
+	Layers    []Layer             `yaml:"layers,omitempty"`    // Layers are only defined in root, first is considered default
+	Variables map[string]Variable `yaml:"variables,omitempty"` // Optionally define what values to expect
 	// The key is the folder path relative to the root? If we use a tree structure here with references
 	// to parent/child we won't be able to serialize.  We can possibly store the Tree as just a tree
 	// of node names with the folder values kept here.
-	Folders map[string]Folder
+	Folders map[string]Folder `yaml:"folders,omitempty"`
 }
 
 type Variable struct {
-	Type       string
-	Validation string
-	Default    interface{}
+	Type       string      `yaml:"type,omitempty"`
+	Validation string      `yaml:"validation,omitempty"`
+	Default    interface{} `yaml:"default,omitempty"`
 }
 
 type Layer struct {
-	Levels []string
-	Match  string // Determines which layer is selected?
+	Levels []string `yaml:"levels,omitempty"`
+	Match  string   `yaml:"match,omitempty"` // Determines which layer is selected?
 }
