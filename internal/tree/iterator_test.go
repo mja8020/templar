d01378923ae4b5759cfab271a2bab550fbaf65c8
mja8020 <6097343+mja8020@users.noreply.github.com>
2024-10-31 17:30:32 +0000

    Fix for different paths when testing

--- internal/tree/iterator_test.go
+++ internal/tree/iterator_test.go
@@ -1,6 +1,7 @@
 package tree
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -19,17 +20,22 @@ func TestIterator(t *testing.T) {
 	   f -> /e/f -> /workspaces/Templar/fixtures/1_basic/e/f
 	*/
 
-	tree, err := NewTree("../../fixtures/1_basic")
+	fixture := "../../fixtures/1_basic"
+	path, err := filepath.Abs(fixture)
 	require.NoError(t, err)
 
+	tree, err := NewTree(fixture)
+	require.NoError(t, err)
+
+	// Full path depends where test is running from
 	expecting := [][]string{
-		{"", "/", "/workspaces/Templar/fixtures/1_basic", ""},
-		{"a", "/a", "/workspaces/Templar/fixtures/1_basic/a", "/"},
-		{"b", "/a/b", "/workspaces/Templar/fixtures/1_basic/a/b", "/a"},
-		{"c", "/a/c", "/workspaces/Templar/fixtures/1_basic/a/c", "/a"},
-		{"d", "/a/c/d", "/workspaces/Templar/fixtures/1_basic/a/c/d", "/a/c"},
-		{"e", "/e", "/workspaces/Templar/fixtures/1_basic/e", "/"},
-		{"f", "/e/f", "/workspaces/Templar/fixtures/1_basic/e/f", "/e"},
+		{"", "/", path, ""},
+		{"a", "/a", filepath.Join(path, "a"), "/"},
+		{"b", "/a/b", filepath.Join(path, "a", "b"), "/a"},
+		{"c", "/a/c", filepath.Join(path, "a", "c"), "/a"},
+		{"d", "/a/c/d", filepath.Join(path, "a", "c", "d"), "/a/c"},
+		{"e", "/e", filepath.Join(path, "e"), "/"},
+		{"f", "/e/f", filepath.Join(path, "e", "f"), "/e"},
 	}
 
 	for i, n := range tree.Iterator() {
